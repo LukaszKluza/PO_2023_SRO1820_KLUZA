9d81f873e71e074028d0e2f7c8483ef7c7c74a0f
unknown <lukasz.9@o2.pl>
2023-10-18 19:44:44 +0200

    Add tasks 1-15

---
+++ GIT_Animals/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,15 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("System wystartował");
+        run(args);
+        System.out.println("System zakończył działanie");
+    }
+    public static void run(String[] args){
+        System.out.println("Zwierzak idzie do przody");
+        for(int i = 0; i < args.length; ++i){
+            
+        }
+    }
+}
